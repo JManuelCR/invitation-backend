2ba8353acc91f19a8c81df95a869f3405fb3e719
Manuel Cabrera <josemanuelcabrera2011@gmail.com>
2025-08-18 21:55:16 -0600

    change 7guest model

--- src/models/guest.model.js
+++ src/models/guest.model.js
@@ -3,20 +3,13 @@ const mongoose = require("mongoose");
 
 
 const guestSchema = new mongoose.Schema({
-    guestFrom: {
-        type: String,
-        required: true,
-        default: "Novio y Novia",
-        enum: ["Novio y Novia", "Novio", "Novia", "Amigo", "Amiga", "Familiar", "Otro"],  
-    },
     guestType: {
         type: String,
         required: false,
     },
     guestRelationship: {
-        type: Number,
+        type: String,
         required: false,
-        enum: ["Padre", "Madre", "Hermano", "Hermana", "Tio", "Tia", "Sobrino", "Sobrina", "Primo", "Prima", "Amigo", "Amiga", "Familiar", "Otro"],
     },
     guestName: {
         type: String,
@@ -26,78 +19,93 @@ const guestSchema = new mongoose.Schema({
         type: Number,
         required: true,
         min: 0,
-        max: 6,
+        max: 8,
         default: 0
     },
-    guestPeopleNumber:{
-        type: Number,
-        required: true,
-        min: 0,
-        max: 6,
-        default: 0
-    },
-    guestInvitation: {
-        type: Boolean,
-        required: true,
-    },
-    guestWouldAccept: {
+    guestIsInvited: {
         type: Boolean,
         required: true,
         default: false
     },
-    guestExpectationAssistant: {
-        type: Number,
-        required: false,
-        min: 0,
-        max: 6,
-        default: 0
-    },
-    priority:{
-        type: Number,
-        required: false,
-        default: null
-    },
-    guestCanBeDeleted: {
-        type: Boolean,
-        required: true,
-        default: false
-    },
-    guestInvitationName: {
+    guestInvited: {
         type: String,
-        required: true,
+        required: false,
+        enum: ["YES", "NO","MAYBE"],
+        default: "NO"
+    },
+    guestSide: {
+        type: String,
+        required: false,
+        default: "Ambos"
+    },
+    guestProbability: {
+        type: Number,
+        required: false,
+        min: 0,
+        max: 6,
+        default: 0
+    },
+    guestPriority: {
+        type: Number,
+        required: false,
+        default: 0
+    },
+    guestInvitationDelivered: {
+        type: Boolean,
+        required: false,
+        default: false
+    },
+    guestTransportCount: {
+        type: Number,
+        required: false,
+        min: 0,
+        default: 0
+    },
+    guestForeigner: {
+        type: String,
+        required: false,
+        enum: ["YES", "NO"],
+        default: "NO"
+    },
+    guestChurchAssistantConfirmation: {
+        type: Boolean,
+        required: false,
+        default: false
+    },
+    guestReceptionAssistantConfirmation: {
+        type: Boolean,
+        required: false,
+        default: false
     },
     guestPrimaryContact: {
         type: String,
         required: true,
         unique: true,
         minlength: 10,
-        maxlength: 15,
+        maxlength: 20,
         validate: {
             validator: function (v) {
-                return v === null || /^\+\d{10,15}$/.test(v);
+                return v === null || /^\+?\d{10,20}$/.test(v);
               },
-              message: 'El número debe estar en formato internacional (ej. +5215512345678) y tener entre 10 y 15 dígitos.'  
+              message: 'El número debe estar en formato internacional (ej. +52 554 026 19 32) y tener entre 10 y 13 dígitos.'  
         }
     },
     guestSecondaryContact: {
         type: String,
         required: false,
         minlength: 10,
-        maxlength: 15,
+        maxlength: 20,
         default: null,
         validate: {
             validator: function (v) {
-                return v === null || /^\+\d{10,15}$/.test(v);
+                return v === null || /^\+?\d{10,20}$/.test(v);
               },
-              message: 'El número debe estar en formato internacional (ej. +5215512345678) y tener entre 10 y 15 dígitos.'  
+              message: 'El número debe estar en formato internacional (ej. +52 554 026 19 32) y tener entre 10 y 13 dígitos.'  
         }
     },
     guestPhysicalInvitation: {
         type: Boolean,
-        default: false
-    },
-    guestWasInvited: {
-        type: Boolean,
+        required: true,
         default: false
     },
     guestInvitationResponse: {
@@ -108,25 +116,31 @@ const guestSchema = new mongoose.Schema({
         type: Number,
         required: false,
         min: 0,
-        max: 6,
+        max: 8,
         default: 0
     },
     guestLanguage: {
         type: String,
         required: false,
-        enum: ["Español", "Inglés", "Portugués", "Francés", "Alemán", "Italiano", "Otro"],
-        default: "Español"
+        enum: ["Espanol", "Ingles"],
+        default: "Espanol"
     },
     guestInvitationId: {
         type: String,
         required: true,
         unique: true
     },
-    guestFoodType:{
-        type: String,
+    guestPorkCountDesire: {
+        type: Number,
         required: false,
-        enum: ["Pollo", "Cerdo"],
-        default: "Pollo"
+        min: 0,
+        default: 0
+    },
+    guestChickenCountDesire: {
+        type: Number,
+        required: false,
+        min: 0,
+        default: 0
     }
 })
 
